1b4b205b661de3abe1d1b186b2e688a2bf6a0b15
madhukar eppalapelly <madhukar.eppalapelly@gmail.com>
2023-01-18 00:15:00 +0530

    first commit

--- APIS/DataApi.js
+++ APIS/DataApi.js
@@ -1,9 +1,10 @@
 const exp = require("express");
 const userApp = exp.Router();
-const mongoClient = require("mongodb").MongoClient;
-const dbConnectionString = "mongodb+srv://madhu:madhu@clusterbackend.szevd.mongodb.net/myfirstdb?retryWrites=true&w=majority"
-let userCollectionObject;
-let dataCollectionObject;
+const requestIp = require('request-ip');
+const ipfetch = require('ip-fetch');
+const mongoClient = require('mongodb').MongoClient;
+const dbConnectionString  = "mongodb+srv://madhu:madhu@clusterbackend.szevd.mongodb.net/myfirstdb?retryWrites=true&w=majority"
+let messageCollection, countCollection, locationCollection;
 
 
 mongoClient.connect(dbConnectionString)
@@ -11,8 +12,7 @@ mongoClient.connect(dbConnectionString)
     //create DB object
     const dbObj = client.db("portfolio");
     //get collection object
-    
-    dataCollectionObject = dbObj.collection("messageData")
+    messageCollection = dbObj.collection("messageData")
     //share userCollectionObj
     console.log("Connected to messageDB ")
   })
@@ -26,28 +26,77 @@ mongoClient.connect(dbConnectionString)
     const dbObj = client.db("portfolio");
     //get collection object
     
-    userCollectionObject = dbObj.collection("track")
+    countCollection = dbObj.collection("track")
     //share userCollectionObj
     console.log("Connected to dataDB ")
   })
   .catch(err => console.log("err in connecting to DB ", err))
 
 
+
+  mongoClient.connect(dbConnectionString)
+  .then(client => {
+    //create DB object
+    const dbObj = client.db("portfolio");
+    //get collection object
+    locationCollection = dbObj.collection("locationData")
+    //share userCollectionObj
+    console.log("Connected to locationDB ")
+  })
+  .catch(err => console.log("err in connecting to DB ", err))
+
+
+
+
+
+
 //middleware to parse  body of req
 userApp.use(exp.json());
 
+//define routes
+//route for GET req for all users
+userApp.get("/data", async (request, res) => {
+  //get usercollectionobj
+  //get data
+  //send res
+  userApp.use(requestIp.mw());
+  const clientIp = await request.clientIp;
+  let info = await ipfetch.getLocationNpm("124.123.186.218");
+  
+  reqObj =
+  {
+      "country": info.country,
+      "region": info.region,
+      "regionName": info.regionName,
+      "city": info.city,
+      "zip": info.zip,
+      "lat": info.lat,
+      "lon": info.lon,
+      "org": info.org,
+      "as": info.as,
+      "query": info.query
+  }
+  try {
+      let response = await locationCollection.insertOne(reqObj);
+  } catch (e) {
+      console.log("error in insertion", e);
+  }
 
+  res.send({ message:clientIp})
+});
+
+
+
+/*----------------------------------------------------------------------------------*/
 //route for POST req
 userApp.post("/create-user", async (request, response) => {
   //get usercollectionobj
   //get userObj from client
   let userObj = await request.body;
-
   //verify existing user
-  let userOfDB = await dataCollectionObject.findOne({
+  let userOfDB = await messageCollection.findOne({
     username: userObj.username,
   });
-
 
   //if user existed
   if (userOfDB !== null) {
@@ -55,44 +104,68 @@ userApp.post("/create-user", async (request, response) => {
   }
   //if user not existed
   else {
-    await dataCollectionObject.insertOne(userObj);
+    await messageCollection.insertOne(userObj);
     response.send({ message: "Message Sent" });
   }
 });
+/*----------------------------------------------------------------------------------*/
 
 
 
 
 
+/*----------------------------------------------------------------------------------*/
 userApp.post("/track", async (request, response) => {
 
   //get userObj from client
   let userObj = await request.body;
 
   //verify existing date
-  let userOfDB = await userCollectionObject.findOne({
+  let userOfDB = await countCollection.findOne({
     currentDate: userObj.currentDate
   });
 
   //if date existed update
   if (userOfDB !== null) {
     userObj = { ...userObj, count: userOfDB.count + 1 }
-    let res = await userCollectionObject.updateOne(
+    let res = await countCollection.updateOne(
       { currentDate: userObj.currentDate },
       { $set: { ...userObj } }
     );
-    
+
     //response.send({ message: "Message Sentvhgvghhgfhj" });
   }
   //if user not existed add new
   else {
-    await userCollectionObject.insertOne(userObj);
-   // response.send({ message: "Message Sent" });
+    await countCollection.insertOne(userObj);
+    // response.send({ message: "Message Sent" });
     //send res
   }
 
 });
 
+/*----------------------------------------------------------------------------------*/
+
 
 //export userApp
 module.exports = userApp;
+
+
+
+
+
+/*
+
+
+//connect to mongodb
+const client = new MongoClient(uri, { useNewUrlParser: true });
+try {
+   client.connect();
+  const dbObj = client.db("portfolio");
+  locationCollection = dbObj.collection("locationData")
+  countCollection = dbObj.collection("track")
+  messageCollection = dbObj.collection("messageData")
+} catch (err) {
+  console.log("error in connecting mongoDB", err);
+}
+*/
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 const requestIp = require('request-ip');
-const ipfetch = require('ip-fetch');
-const mongoClient = require("mongodb").MongoClient;
-const dbConnectionString = "mongodb+srv://madhu:madhu@clusterbackend.szevd.mongodb.net/myfirstdb?retryWrites=true&w=majority"
-let dataCollectionObject;
 
 //import userApp&productApp
 const dataApi = require("./APIS/DataApi");
@@ -17,58 +13,53 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-async function connectToMongo() {
-    try {
-        const client = await mongoClient.connect(dbConnectionString, { useNewUrlParser: true });
-        //create DB object
-        const dbObj = client.db("portfolio");
-        //get collection object
-
-        dataCollectionObject = dbObj.collection("locationData")
-        //share userCollectionObj
-        console.log("Connected to locationDB ")
-
-
-    } catch (error) {
-        console.log("err in connecting to DB ", error);
-    }
-}
-
 
 //execute routes based on path
 app.use("/user", dataApi)
 
-app.use(requestIp.mw());
-app.use(async (req, res) => {
-    const clientIp = req.clientIp;
-    let info = await ipfetch.getLocationNpm("124.123.186.218");
-    reqObj =
-    {
-        "country": info.country,
-        "region": info.region,
-        "regionName": info.regionName,
-        "city": info.city,
-        "zip": info.zip,
-        "lat": info.lat,
-        "lon": info.lon,
-        "org": info.org,
-        "as": info.as,
-        "query": info.query
-    }
-    try {
-       await  connectToMongo()
-        let response = await dataCollectionObject.insertOne(reqObj);
-        if (response.acknowledged === true) {
-           
-           // res.send({ message: "successful" })
-        }
-        else {
-              // res.send({ message: "unsuccesful" })
-        }
-    } catch (e) {
-        console.log("error in insertion", e);
-    }
+app.get("/", async(req, res) => {
+
+    
+  app.use(requestIp.mw());
+  const clientIp = await req.clientIp;
+res.send({message:clientIp})
 
 });
 const port = 5000;
 app.listen(port, () => console.log("server on port 5000..."))
+
+
+
+
+/*
+
+    app.use(requestIp.mw());
+        const clientIp = await req.clientIp;
+        
+        let info = await ipfetch.getLocationNpm("124.123.186.218");
+        console.log(info)
+        reqObj =
+        {
+            "country": info.country,
+            "region": info.region,
+            "regionName": info.regionName,
+            "city": info.city,
+            "zip": info.zip,
+            "lat": info.lat,
+            "lon": info.lon,
+            "org": info.org,
+            "as": info.as,
+            "query": info.query
+        }
+        console.log(reqObj)
+        try {
+            await connectToMongo()
+            let response = await dataCollectionObject.insertOne(reqObj);
+            console.log(response)
+        } catch (e) {
+            console.log("error in insertion", e);
+        }
+
+        res.send({ message: "https://madhukar-eppalapelly.netlify.app" })
+
+        */
